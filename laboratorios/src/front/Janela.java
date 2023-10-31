6f3cdcbd2907de5813d5857e3307e5c308ab21f0
pedrot2910 <127685912+pedrot2910@users.noreply.github.com>
2023-10-31 16:45:09 -0300

    Add files via upload

--- laboratorios/src/front/Janela.java
+++ laboratorios/src/front/Janela.java
@@ -1,11 +1,18 @@
 package front;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Janela extends JFrame {
 
@@ -17,6 +24,8 @@ public class Janela extends JFrame {
 	Painel painelsuperior = new Painel(Color.BLUE);
 	Painel painelinferior = new Painel(Color.CYAN);
 	Painel painelintermed = new Painel(Color.BLACK);
+	
+	CardLayout cardlay = new CardLayout();
 	
 	public Janela() {
 		setSize(800, 600);
@@ -36,7 +45,48 @@ public class Janela extends JFrame {
 		painelsuperior.add(botao4);
 		
 		add(painelinferior, BorderLayout.CENTER);
-		
+		painelinferior.setLayout(cardlay);
+		botao1.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Painel painelvendas = new Painel(Color.YELLOW);
+				JTextField campoNome = new JTextField(20);
+				JTextField campoMatri = new JTextField(6);
+				JComboBox<String> comboBoxQ = new JComboBox();
+				JComboBox<String> comboBoxP = new JComboBox();
+				JComboBox<String> comboBoxB = new JComboBox();
+				JLabel labelNome = new JLabel();
+				labelNome.setForeground(Color.RED);
+				labelNome.setText("Nome Cliente: ");
+				comboBoxQ.addItem("Coalho");
+				comboBoxP.addItem("Porco");
+				comboBoxB.addItem("Coca-Cola");
+				
+				painelvendas.add(labelNome);
+				painelvendas.add(campoNome);
+				painelvendas.add(new JLabel("Matricula: "));
+				painelvendas.add(campoMatri);
+				painelvendas.add(new JLabel("Opção de queijo: "));
+				painelvendas.add(comboBoxQ);
+				painelvendas.add(new JLabel("Opção de proteina: "));
+				painelvendas.add(comboBoxP);
+				painelvendas.add(new JLabel("Escolha a opção de queijo: "));
+				painelvendas.add(new JCheckBox("Maionese"));
+				painelvendas.add(new JLabel(""));
+				painelvendas.add(new JCheckBox("Ketchup"));
+				painelvendas.add(new JLabel(""));
+				painelvendas.add(new JCheckBox("Ovo"));
+				painelvendas.add(new JLabel(""));
+				painelvendas.add(new JCheckBox("Batata palha"));
+				painelvendas.add(new JLabel("Bebidas: "));
+				painelvendas.add(comboBoxB);
+				painelvendas.setLayout(new GridLayout(10,10));
+				
+				cardlay.show(painelinferior, "Vendas");
+				painelinferior.add(painelvendas);
+			}
+		});
 		
 	}
 }
